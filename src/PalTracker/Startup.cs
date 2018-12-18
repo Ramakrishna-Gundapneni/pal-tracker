7df40e3efde40c41a96cdefdd621b95485008868
Samudral <Kiran_Kumar_Samudral@dell.com>
2018-12-18 12:51:00 +0530

    Pipeline configuration changes

---
+++ src/PalTracker/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace PalTracker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage(
+Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
+            services.AddSingleton(sp => new CloudFoundryInfo(
+                Configuration.GetValue<string>("Port ", "PORT not configured."),
+                Configuration.GetValue<string>("MemoryLimit  ", "MemoryLimit not configured."),
+             Configuration.GetValue<string>("CfInstanceIndex  ", "CfInstanceIndex  not configured."),
+              Configuration.GetValue<string>("CfInstanceAddr   ", "CfInstanceAddr   not configured.")
+                ));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
